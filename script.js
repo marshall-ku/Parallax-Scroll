bed530634a9e1c2e1645099156c83ea077cbc664
Marshall <taengoo890309@naver.com>
2019-04-29 23:20:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const scr = $(".scr-down");
+
+$.fn.parscroll = function (spd) {
+    const opt = $.extend({}, spd);
+    opt.speed===void 0&&(opt.speed=.3);
+    this.each(function() {
+        const t = $(this);
+
+        function init() {
+            const title = t.data("title");
+            t.append(`<div class="bg-wrap"><div class="bg" style="background-image:url(${t.data("image")})"></div></div>`)
+    
+            if (title !== undefined) {
+                t.prepend(`<div class="item-title">${title}</div>`)
+            }
+        }
+        function scroll() {
+            const scrolltop = $(window).scrollTop(),
+                     offsettop = t.offset().top;
+    
+            if (offsettop + t.outerHeight() > scrolltop && scrolltop + $(window).height() > offsettop) {
+                t.find(".bg").css("transform", `translateY(${-opt.speed * (scrolltop - offsettop)}px)`)
+            }
+        }
+        init(), scroll(), $(window).scroll(scroll)
+    })
+}
+
+$(".parallax-item").parscroll(),
+$(window).scroll(function() {
+    if(0!==$(window).scrollTop() ) {
+        scr.addClass("hide")
+    }
+    else {
+        scr.removeClass('hide')
+    }
+}),
+$(".smt-scr").on("click", function(f) {
+    f.preventDefault();
+    var h = this.hash;
+    $("html, body").animate({
+      scrollTop: $(h).offset().top
+    }, 800)
+}),
+$(".button[lang]").click(function() {
+    $("html").toggleClass("korean")
+})
